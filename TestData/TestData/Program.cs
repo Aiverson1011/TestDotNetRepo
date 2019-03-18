a4b1fd0185d3bb6733b1fc1c703894949e5caff7
Amanda Iverson <ajohnson764@gmail.com>
2019-03-18 15:52:17 -0700

    added a new console app

---
+++ TestData/TestData/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void TestData()
+        {
+            Console.WriteLine("This is super duper cool");
+        }
+    }
+}
